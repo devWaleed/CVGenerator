cfd36141dd6ff0e3df56e94f601c6833666537e1
Waleed <devwaleed@gmail.com>
2021-09-09 00:12:13 +0500

    lots of ui changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,116 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import JobCard from "./JobCard";
+import JobCard, { Project } from "./JobCard";
 
 export default function App() {
-  const [jobs, setJobs] = useState([1]);
+  const [jobs, setJobs] = useState([
+    {
+      organization: "Aciano Technologies",
+      roles: [
+        {
+          title: "Team Lead Mobile Apps Division (React Native)",
+          description:
+            "Leading a team of React Native developers. I am responsible for management of my team which includes requirements discussion, task management, hiring new resources as needed and training each individual where necessary.",
+          duration: "Oct 2020 - Present - 1 yr",
+          projects: [],
+        },
+        {
+          title: "Senior Software Engineer (JazzCash Mobile App division)",
+          description:
+            "As a senior software engineer in JazzCash team, I was assigned to migrate old code base of JazzCash app to latest stable version. We updated whole app to the latest version of Xamarin Forms for both Android and iOS platforms. I also contributed in the development of few new modules which were later deployed to production successfully.",
+          duration: "Feb 2020 - Sep 2020 - 8 mos",
+          projects: [
+            {
+              name: "JazzCash - Money Transfer, Mobile Load & Payments - Apps on Google Play",
+              link: "https://play.google.com/store/apps/details?id=com.techlogix.mobilinkcustomer&hl=en_US&gl=US",
+              description:
+                "JazzCash is Pakistan’s #1 mobile account trusted by millions of Pakistanis. Download JazzCash app to transfer money to anyone anywhere, pay utility bills such as electricity, internet, gas & water, \n get mobile recharge and make payments to hundreds of companies and institutes all across Pakistan in just a few taps.",
+              skills: ["C# .NET", "Xamarin Forms", "API Integration"],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      organization: "Avolox Pvt Ltd",
+      roles: [
+        {
+          title: "Full Stack Team Lead (.NET, React, React Native)",
+          description:
+            "Led a team of multiple developers for web and mobile application development. I contributed as a full stack application developer mainly using .NET and React.js technologies. My other responsibilities included team building and product UX development.",
+          duration: "Sep 2018 - Jan 2020 - 1 yr 5 mos",
+
+          projects: [
+            {
+              name: "iTecknologi - App on Google Play",
+              link: "https://play.google.com/store/apps/details?id=com.avolox.iteck&hl=en_US&gl=US",
+              description:
+                "iTecknologi Group is an innovation driven business group focuses on bringing leading technologies to rapidly developing customer base in Pakistan. The group consists of 6 member companies operating in six countries",
+              skills: [
+                "C# .NET",
+                "ASP.NET Core Web API",
+                "SignalR",
+                "Google Maps",
+                "React Native",
+                "Lots of Maths",
+              ],
+            },
+            {
+              name: "Salaam Takaful - App on Google Play",
+              link: "https://play.google.com/store/apps/details?id=com.avolox.salaamtakaful&hl=en_US&gl=US",
+              description:
+                "Does keeping track of your health feel unnecessary with life being so complicated and busy? Gone are the days! Now you can stay at the top of your health activities. Salaam Takaful Limited’s “Salaam App,” offers you some of the world's best hi-tech...                ",
+              skills: [
+                "C# .NET",
+                "ASP.NET Core Web API",
+                "SignalR",
+                "React Native",
+              ],
+            },
+            {
+              name: "iTecknologi | Mobile Daily Report",
+              link: "http://iteck.pk/d/rndmlnk1",
+              description:
+                "Does keeping track of your health feel unnecessary with life being so complicated and busy? Gone are the days! Now you can stay at the top of your health activities. Salaam Takaful Limited’s “Salaam App,” offers you some of the world's best hi-tech...                ",
+              skills: [
+                "C# .NET",
+                "ASP.NET Core Web API",
+                "SignalR",
+                "Vue.js",
+                "Google Maps",
+                "Lots of Maths",
+              ],
+            },
+          ],
+        },
+        {
+          title: "Full Stack Developer (.NET & Vue.js)",
+          description:
+            "I contributed to the development of few web applications in this role which were mainly based on ASP .NET and other related technologies",
+          duration: "Feb 2018 - Aug 2018 - 7 mos",
+          projects: [
+            {
+              name: "Paperbucketmdb - Easy campaign management with analytics and AI",
+              link: "https://www.paperbucketmdb.com/",
+              description:
+                "Was part of the early development cycle and contributed to some major parts of the product.",
+              skills: [
+                "C# .NET",
+                "ASP.NET Core Web API",
+                "SignalR",
+                "Vue.js",
+                "Google Maps",
+                "Lots of Maths",
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
   return (
-    <View style={{ paddingHorizontal: "10%" }}>
+    <View style={{ marginHorizontal: "10%" }}>
       <View style={{ flexDirection: "row", paddingVertical: 20 }}>
         <View
           style={{
@@ -14,7 +118,7 @@ export default function App() {
             justifyContent: "center",
           }}
         >
-          <Text style={{ fontSize: 45 }}>Your Name Here</Text>
+          <Text style={{ fontSize: 45 }}>Waleed Baig</Text>
         </View>
         <View
           style={{
@@ -23,24 +127,67 @@ export default function App() {
             alignItems: "center",
           }}
         >
-          <Text>+12345 6789101</Text>
+          <Text>0334 3954568</Text>
           <Text>devwaleed@gmail.com</Text>
         </View>
       </View>
       <View>
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Experienced individual with a background of startup team management
+          and over 5 years of full stack software development. Seeking to
+          leverage my leadership expertise in order to maximize development
+          efficiency through agile methodologies.
         </Text>
       </View>
-      {jobs.map((item, index) => (
-        <JobCard key={index} />
-      ))}
+
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 10 }}>
+          Experience
+        </Text>
+        {jobs.map((item, index) => (
+          <View style={{ marginTop: 10 }}>
+            <Text
+              style={{ marginVertical: 5, fontSize: 14, fontWeight: "bold" }}
+            >
+              {item.organization}
+            </Text>
+            {item.roles.map((item2, index) => (
+              <View style={{ marginLeft: 10, marginTop: 0 }}>
+                <JobCard
+                  key={index}
+                  title={item2.title}
+                  description={item2.description}
+                  projects={item2.projects}
+                  duration={item2.duration}
+                />
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+      <View>
+        <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 10 }}>
+          Projects
+        </Text>
+        {jobs.map((item, index) => (
+          <View style={{ marginTop: 10 }}>
+            {item.roles.map((item2, index) => (
+              <View style={{ marginLeft: 10, marginTop: 0 }}>
+                {item2.projects.map((item3, index) => (
+                  <View style={{ marginVertical: 10 }}>
+                    <Project
+                      name={item3.name}
+                      link={item3.link}
+                      description={item3.description}
+                      skills={item3.skills}
+                    />
+                  </View>
+                ))}
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
--- src/JobCard.js
+++ src/JobCard.js
@@ -1,46 +1,22 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 
-export default function JobCard() {
-  const [description, setDescription] = useState(
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" +
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" +
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-  );
-
-  const [projects, setProjects] = useState([
-    {
-      name: "Example Project",
-      link: "https://play.google.com/",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, \n sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      skills: ["C#", ".NET", "Xamarin Forms", "API Integration"],
-    },
-  ]);
-
+export default function JobCard({ title, description, projects, duration }) {
   return (
-    <View style={{ marginVertical: 10 }}>
-      <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-        Organization Name Here
-      </Text>
-      <Text style={{ fontWeight: "bold", fontSize: 13 }}>Your Role Here</Text>
-      <Text style={{ marginTop: 5 }}>Feb 2020 - December 2021 (2 years)</Text>
+    <View style={{ marginVertical: 5 }}>
+      <Text style={{ fontWeight: "bold", fontSize: 12 }}>{title}</Text>
+      <Text style={{ marginTop: 5 }}>{duration}</Text>
       <Text style={{ marginTop: 5 }}>{description}</Text>
-      <View style={{ marginTop: 20 }}>
-        <Text style={{ fontWeight: "bold" }}>Projects</Text>
-        {projects.map((item, index) => (
-          <View style={{ marginTop: 10, flexDirection: "row" }}>
-            <Text
-              style={{
-                fontWeight: "bold",
-              }}
-            >
-              {index + 1}.{" "}
-            </Text>
-            <Project key={index} {...item} />
-          </View>
-        ))}
-      </View>
+      {/* {projects?.length > 0 && (
+        <View style={{ marginTop: 20, marginLeft: 10 }}>
+          <Text style={{ fontWeight: "bold" }}>Projects</Text>
+          {projects?.map((item, index) => (
+            <View style={{ marginTop: 10, flexDirection: "row" }}>
+              <Project key={index} {...item} />
+            </View>
+          ))}
+        </View>
+      )} */}
     </View>
   );
 }
@@ -50,7 +26,13 @@ export function Project({ name, link, description, skills = [] }) {
     <View>
       <a
         target={"_blank"}
-        style={{ fontWeight: "bold", textDecoration: "none", color: "#000" }}
+        style={{
+          fontWeight: "bold",
+          textDecoration: "none",
+          color: "#000",
+          fontSize: 15,
+          fontFamily: "Georgia",
+        }}
         href={link}
       >
         {name}
@@ -62,11 +44,13 @@ export function Project({ name, link, description, skills = [] }) {
             style={{
               paddingVertical: 3,
               paddingHorizontal: 7,
-              borderRadius: 10,
-              backgroundColor: "orange",
-              color: "#000",
+              borderRadius: 5,
+              backgroundColor: "#141414",
+              color: "#fff",
               marginRight: 5,
-              fontSize: 12,
+              fontSize: 10,
+              fontStyle: "italic",
+              fontWeight: "bold",
             }}
             key={index}
           >
