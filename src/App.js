c7be6f39b6deb60e636594a1a65e28c357d85157
WaleedBaigAciano <waleed.baig@aciano.net>
2021-09-08 14:22:56 +0500

    git init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+class App extends React.Component {
+  render() {
+    return <Text>Hello World</Text>;
+  }
+}
+
+export default App;
